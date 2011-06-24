08285526e1093a54114087e3207f0bbf3d27ee76
Fernando Espinosa <fer@ferdev.com>
2011-06-24 18:02:07 +0200

    First commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'sass', '~> 3.1.3'
+gem 'pg', '0.11.0'
+
+group :development do
+  gem 'capistrano', '~> 2.6.0'
+  gem 'capistrano-ext', '~> 1.2.1'
+end
+
+group :development, :test do
+  gem 'rspec-rails', '~> 2.4'
+  gem 'capybara', '~> 1.0.0'
+  gem 'ruby-debug19', :platforms => :mri_19
+  gem 'foreman', '~> 0.18.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Irekia::Application.config.secret_token = 'fee35727c965b34a5e5d5200eb53a041b231a095cd5404b5a90c900ed93423051d79ac591d8d04c3e4ad837753b5e34163106ac43b0bd58b9ef3894bc24d5f54'
