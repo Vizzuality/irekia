39813afbd4583f6935c201b8ab17e29ce469d085
Fernando Espinosa <fer@ferdev.com>
2011-06-28 19:28:59 +0200

    Adds first roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+# Roles creation
+Role.find_or_create_by_name('Admin')
+Role.find_or_create_by_name('Politician')
+Role.find_or_create_by_name('Citizen')
