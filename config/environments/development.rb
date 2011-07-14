d2952a2b2518272862e1bab95ded2069f111b1b9
Fernando Espinosa <fer@ferdev.com>
2011-07-14 19:15:03 +0200

    Changes some mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Irekia::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = {
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -15,17 +15,16 @@ Irekia::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = {
-    :host => "irekia-stage.ipq.co",
-    :port => 3000
+    :host => "irekia-stage.ipq.co"
   }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => 'ferdev.com',
-    :user_name            => 'test',
+    :user_name            => 'test@ferdev.com',
     :password             => 'vizzuality',
     :authentication       => 'plain',
     :enable_starttls_auto => true
