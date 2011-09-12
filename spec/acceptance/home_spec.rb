96432493574084edcdd84d8eeec2b0320acd46a5
Javier Arce <javierarce@gmail.com>
2011-09-12 17:26:00 +0200

    Application uses html5 layout. Home specs updated to reflect those changes

--- spec/acceptance/home_spec.rb
+++ spec/acceptance/home_spec.rb
@@ -6,7 +6,7 @@ feature "Home" do
   scenario "has a layout common to all pages" do
     visit root_path
 
-    within '#header' do
+    within 'header' do
       page.should have_css 'img', :src => '/images/irekia_logo.jpg'
       page.should have_css 'img', :src => '/images/euskadi_net_logo.jpg'
       within 'ul' do
@@ -18,41 +18,43 @@ feature "Home" do
       end
     end
 
-    within '#navigation' do
-      page.should have_content 'Estás viendo...'
-      page.should have_css 'ul.areas'
-      page.should have_css 'ul.areas li', :text => 'Educación, Universidades e Investigación'
-      page.should have_no_css 'ul.areas li.selected'
+    within 'ul.auth' do
+      page.should have_link 'Regístrate'
+      page.should have_link 'Login'
+    end
 
+    within '#sign_in' do
+      page.should have_css 'h1', :text => 'Accede a Irekia'
+      page.should have_css 'p', :text => '¿Olvidaste tu contraseña? Recupérala aquí'
+      page.should have_link 'Recupérala aquí'
       within 'form' do
-        page.should have_css 'label', :text => 'O busca lo que necesitas...'
+        page.should have_css 'label', :text => 'Email'
         page.should have_css 'input', :type => 'text'
-        page.should have_css 'input', :type => 'submit'
-      end
-
-      within '.auth' do
-        page.should have_link 'Regístrate'
-        page.should have_link 'Login'
-
-        page.should have_css 'h1', :text => 'Accede a Irekia'
-        page.should have_css 'p', :text => '¿Olvidaste tu contraseña? Recupérala aquí'
-        page.should have_link 'Recupérala aquí'
-        within 'form' do
-          page.should have_css 'label', :text => 'Email'
-          page.should have_css 'input', :type => 'text'
-          page.should have_css 'label', :text => 'Contraseña'
-          page.should have_css 'input', :type => 'text'
-          page.should have_css 'input', :type => 'checkbox'
-          page.should have_css 'label', :text => 'Recordar mis datos'
-          page.should have_css 'input', :type => 'submit', :value => 'Entrar'
-        end
+        page.should have_css 'label', :text => 'Contraseña'
+        page.should have_css 'input', :type => 'text'
+        page.should have_css 'input', :type => 'checkbox'
+        page.should have_css 'label', :text => 'Recordar mis datos'
+        page.should have_css 'input', :type => 'submit', :value => 'Entrar'
       end
     end
 
-    page.should have_css '#content'
+    within 'nav' do
+      page.should have_content 'Estás viendo...'
+      page.should have_css 'ul.areas'
+      page.should have_css 'ul.areas li', :text => 'Educación, Univ...'
+      page.should have_no_css 'ul.areas li.selected'
 
-    within '#footer' do
-      within '.copyright' do
+      within 'form' do
+        #page.should have_css 'label', :text => 'O busca lo que necesitas...'
+        page.should have_css 'input', :type => 'text'
+        page.should have_css 'input', :type => 'submit'
+      end
+    end
+
+    page.should have_css '#container'
+
+    within 'footer:last-child .content' do
+      within 'ul.copyright' do
         page.should have_css 'img', :src => '/images/logo.jpg'
         page.should have_link 'Información legal'
         page.should have_content '© 2011 Eusko Jaurlaritza'
@@ -79,6 +81,8 @@ feature "Home" do
           page.should have_link 'Economía y Hacienda'
           page.should have_link 'Justicia y Admon. Pública'
           page.should have_link 'Vivienda'
+        end
+        within 'ul:last-child' do
           page.should have_link 'Obras Públicas y Transportes'
           page.should have_link 'Industria e Innovación'
           page.should have_link 'Comercio y Turismo'
