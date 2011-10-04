83a1a271ce57a1ce497898220a431ba416514c6c
Fernando Espinosa <fer@ferdev.com>
2011-10-04 17:22:11 +0200

    Shows events in agenda

--- app/helpers/areas_helper.rb
+++ app/helpers/areas_helper.rb
@@ -17,25 +17,4 @@ module AreasHelper
     agenda_area_path(@area)
   end
 
-  def agenda_for_day(agenda, day)
-    day_events = agenda.select{|e| e.event_date.to_date.eql?(day.to_date)}
-    html= []
-    day_events.each do |event|
-      html << content_tag(:div, event.title, :class => 'title')
-
-      if action_name == 'agenda'
-        event_detail = []
-        html << content_tag(:div, :class => 'detail') do
-          event_detail << content_tag(:h4, l(event.event_date, :format => '%d, %B de %Y'))
-          event_detail << content_tag(:h3, event.title)
-          event_detail << content_tag(:div, event.location, :class => 'location')
-          event_detail << content_tag(:div, event.event_date.strftime('%H:%M'), :class => 'time')
-
-          raw event_detail.join
-        end
-      end
-    end
-    html.join
-  end
-
 end
