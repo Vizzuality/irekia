b1532d01b8403d2816d49fa1b66e77b4577f9026
Fernando Espinosa <fer@ferdev.com>
2011-10-19 20:33:52 +0200

    Updates deployment script

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 git checkout staging
-git fetch origin
-git rebase -p master
-git rebase -p origin/staging
+git pull --rebase origin staging
+git merge master
 git push origin staging
 git checkout master
 cap staging deploy
