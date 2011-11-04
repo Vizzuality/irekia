5ca4a21827f50b4c5944162afc8858fdd2c2d63d
Fernando Espinosa <fer@ferdev.com>
2011-11-04 11:26:38 +0100

    Adds all areas

--- db/seeds/data/areas.rb
+++ db/seeds/data/areas.rb
@@ -4,4 +4,20 @@ puts ''.green
 puts 'Creating areas...'.green
 puts '================='.green
 
-@area = create_area :name => 'Educación, Universidades e Investigación'
+create_area :name => 'Presidencia'
+create_area :name => 'Interior'
+@area = create_area :name => 'Educación e Investigación'
+create_area :name => 'Economía y Hacienda'
+create_area :name => 'Justicia'
+create_area :name => 'Adm. Pública'
+create_area :name => 'Vivienda'
+create_area :name => 'Obras Públicas'
+create_area :name => 'Transporte y movilidad'
+create_area :name => 'Industria'
+create_area :name => 'Comercio y Turismo'
+create_area :name => 'Empleo'
+create_area :name => 'Asuntos Sociales'
+create_area :name => 'Medio Ambiente'
+create_area :name => 'Urbanismo y territorio'
+create_area :name => 'Agricultura y Pesca'
+create_area :name => 'Cultura'
