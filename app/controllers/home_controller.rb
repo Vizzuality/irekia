32684aea386d833f69507d68d0719d88f0a4e9c9
Fernando Espinosa <fer@ferdev.com>
2011-11-14 16:25:06 +0100

    Adds actions array and partial in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,19 @@ class HomeController < ApplicationController
   skip_before_filter :authenticate_user!, :only => :index
 
   def index
-    @areas = Area.select([:id, :name]).order(:id).all
+    @areas           = Area.select([:id, :name]).order(:id).all
+    @actions         = AreaPublicStream.order('published_at desc').page(1).per(10)
+    @news_count      = @actions.news.count
+    @questions_count = @actions.questions.count
+    @answers_count   = @actions.answers.count
+    @proposals_count = @actions.proposals.count
+    @arguments_count = @actions.arguments.count
+    @votes_count     = @actions.votes.count
+    @photos_count    = @actions.photos.count
+    @videos_count    = @actions.videos.count
+    @statuses_count  = @actions.status_messages.count
+
+    render :partial => 'shared/actions_list' and return if request.xhr?
   end
 
   def nav_bar_buttons
