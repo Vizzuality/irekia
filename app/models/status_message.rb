3f81ac5e9124df13d3ceeec4b52f24fbbe4954da
Fernando Espinosa <fer@ferdev.com>
2011-11-16 16:59:38 +0100

    Allows politicians to publish status messages

--- app/models/status_message.rb
+++ app/models/status_message.rb
@@ -3,6 +3,8 @@ class StatusMessage < Content
 
   delegate :message, :to => :status_message_data, :allow_nil => true
 
+  accepts_nested_attributes_for :status_message_data
+
   def self.from_area(area)
     joins(:author => :areas).moderated.where('areas.id' => area.id)
   end
