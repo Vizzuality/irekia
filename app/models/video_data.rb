1668481027344ca23df5f9748bcded419402763e
Fernando Espinosa <fer@ferdev.com>
2011-12-07 19:04:10 +0100

    Fixes how videos are imported from vimeo/youtube

--- app/models/video_data.rb
+++ app/models/video_data.rb
@@ -2,22 +2,30 @@ class VideoData < ActiveRecord::Base
   belongs_to :video
   belongs_to :answer_data
 
-  before_save :get_embed_html
+  def youtube_url=(url)
+    write_attribute(:youtube_url, url)
+    if url.present?
+      oembed_json = JSON.parse(open("http://www.youtube.com/oembed?url=#{vimeo_url}&format=json&maxwidth=608").read) rescue nil
 
-  def get_embed_html
-    if youtube_url.present? || vimeo_url.present?
-      if youtube_url.present?
-        oembed_json = JSON.parse(open("http://www.youtube.com/oembed?url=#{vimeo_url}&format=json&maxwidth=608").read) rescue nil
-      elsif vimeo_url.present?
-        oembed_json = JSON.parse(open("http://vimeo.com/api/oembed.json?url=#{vimeo_url}&maxwidth=608").read) rescue nil
-      end
-
-      if oembed_json.present?
-        self.title         = oembed_json['title']
-        self.thumbnail_url = oembed_json['thumbnail_url']
-        self.html          = oembed_json['html']
-      end
+      store_oembed(oembed_json)
     end
   end
-  private :get_embed_html
+
+  def vimeo_url=(url)
+    write_attribute(:vimeo_url, url)
+    if url.present?
+      oembed_json = JSON.parse(open("http://vimeo.com/api/oembed.json?url=#{vimeo_url}&maxwidth=608").read) rescue nil
+
+      store_oembed(oembed_json)
+    end
+  end
+
+  def store_oembed(oembed_json)
+    if oembed_json.present?
+      self.title         = oembed_json['title']
+      self.thumbnail_url = oembed_json['thumbnail_url']
+      self.html          = oembed_json['html']
+    end
+  end
+  private :store_oembed
 end
