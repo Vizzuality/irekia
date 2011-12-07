057f9d311b40b40f63c6fb53d5d3e6808078b7e4
Javier Arce <javierarce@gmail.com>
2011-12-07 16:26:28 +0100

    adds +1 icon

--- public/javascripts/helpers.js
+++ public/javascripts/helpers.js
@@ -398,7 +398,6 @@ jQuery.fn.enablePoliticianTags = function(opt){
   $addInputField = $add.find(".input_field"),
   $addInput      = $add.find('input[type="text"]');
 
-
   $(window).bind('_close.politician_suggest', function() {
     _hidePopover();
   });
@@ -496,14 +495,13 @@ jQuery.fn.enablePoliticianTags = function(opt){
    $response.hide();
    $addInputField.parents("li").before($response);
    $response.fadeIn(speed);
-   $addInput.html("");
  });
 
   $(".autosuggest.mini li").live("click", function() {
-    _hidePopover();
     var pID = $(this).attr("data-id");
 
-    alert(pID);
+    $addInput.val("");
+    _hidePopover();
 
     $add.find(".add_politician #editable_politician_id").attr("value", pID);
     $add.find(".add_politician").submit();
