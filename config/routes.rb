3772864cbd6dc94acd4d2d58850fb8c60abdaad4
Fernando Espinosa <fer@ferdev.com>
2011-12-27 17:58:04 +0100

    Adds translate gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Irekia::Application.routes.draw do
 
+
   if Rails.env.development?
     mount ModerationMailer::Preview => 'mail_view'
+
+    Rails.application.routes.draw do
+      match 'translate' => 'translate#index', :as => :translate_list
+      match 'translate/translate' => 'translate#translate', :as => :translate
+      match 'translate/reload' => 'translate#reload', :as => :translate_reload
+    end
+
   end
 
   scope '(:locale)' do
