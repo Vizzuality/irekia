ba6d6a3ca5009d58018ccda0d0c9b0791f10f096
Fernando Espinosa <fer@ferdev.com>
2012-01-20 21:59:57 +0100

    Shows news title and body if not localized title/body exists

--- app/models/news_data.rb
+++ app/models/news_data.rb
@@ -5,7 +5,7 @@ class NewsData < ActiveRecord::Base
   accepts_nested_attributes_for :image, :allow_destroy => true
 
   def title
-    read_attribute("title_#{I18n.locale}") || (I18n.available_locales - [I18n.locale]).map{|lang| read_attribute("title_#{lang}")}.compact.first || ''
+    read_attribute("title_#{I18n.locale}") || (I18n.available_locales - [I18n.locale]).map{|lang| read_attribute("title_#{lang}")}.compact.first || read_attribute('title') || ''
   end
 
   def title=(new_title)
@@ -13,7 +13,7 @@ class NewsData < ActiveRecord::Base
   end
 
   def body
-    read_attribute("body_#{I18n.locale}") || (I18n.available_locales - [I18n.locale]).map{|lang| read_attribute("body_#{lang}")}.compact.first || ''
+    read_attribute("body_#{I18n.locale}") || (I18n.available_locales - [I18n.locale]).map{|lang| read_attribute("body_#{lang}")}.compact.first || read_attribute('body') || ''
   end
 
   def body=(new_body)
