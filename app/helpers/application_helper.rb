06c0efabe44fbeaa9132d4f9e487200b5166dc81
Fernando Espinosa <fer@ferdev.com>
2012-01-25 05:57:07 +0100

    Reverts change in fields translator helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -312,16 +312,7 @@ module ApplicationHelper
   end
 
   def translate_field(model, field)
-    current_locale_value   = model.send(:"#{field}_#{i18n.locale}")
-    alternate_locale_value = (i18n.available_locales).map{|lang| model.send(:"#{field}_#{lang}")}.compact.first
-    no_translated_value    = model.send(field)
-
-    return current_locale_value   if current_locale_value.present?
-    return alternate_locale_value if alternate_locale_value.present?
-    return no_translated_value    if no_translated_value.present?
-    ''
-  rescue
-    ''
+    model.send(:"#{field}_#{I18n.locale}") || (I18n.available_locales).map{|lang| model.send(:"#{field}_#{lang}")}.compact.first || model.send(field) || '' rescue ''
   end
 
 end
