2a35f42cfa6a1cfae6323e4109470d0c93d0f5d5
Javier Arce <javierarce@gmail.com>
2012-01-30 13:52:49 -0500

    Adds static example

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,23 +3,9 @@ class DemoController < ApplicationController
   skip_before_filter :authenticate_user!
 
   def index
-
   end
 
-  def slideshow
-    render :layout => "slideshow"
-  end
-
-  def datalogger_login
-    render :layout => "datalogger.iphone"
-  end
-
-  def datalogger_publish
-    render :layout => "datalogger.iphone"
-  end
-
-  def datalogger_home
-    render :layout => "datalogger.iphone"
+  def static
   end
 
 end
