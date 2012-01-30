57fd16a3ae200e0b319b43408459b7c282a1ee1e
Fernando Espinosa <fer@ferdev.com>
2012-01-30 19:02:30 +0100

    Restricts login from iphone app only for politicians

--- lib/irekia/strategies/mobile.rb
+++ lib/irekia/strategies/mobile.rb
@@ -3,13 +3,9 @@ module Irekia
     class Mobile < Devise::Strategies::DatabaseAuthenticatable
 
       def authenticate!
-        authentication_hash[:role_id] = Role.politician.first.id
-        super
-      end
-
-      def valid?
         device = AgentOrange::UserAgent.new(request.user_agent).device
-        super && device.is_mobile?
+        authentication_hash[:role_id] = Role.politician.first.id if device.is_mobile?
+        super
       end
 
     end
