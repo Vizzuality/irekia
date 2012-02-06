9fdbdcf032d82ed84f2d1838c450613730a4a9d3
Fernando Espinosa <fer@ferdev.com>
2012-02-06 16:32:45 +0100

    Adds more specific progress indicator for update news publishing dates task

--- lib/irekia/importer.rb
+++ lib/irekia/importer.rb
@@ -486,12 +486,17 @@ module Irekia
       puts '=> Updating news publishing dates'
       news_detail_url = lambda{|news_id| "http://www.irekia.euskadi.net/es/news/#{news_id}.xml"}
 
+      i          = 0
+      news_count = News.where('external_id IS NOT NULL').count
+
       News.where('external_id IS NOT NULL').find_each do |news|
         news.cancel_notifications = true
 
         news_detail = Nokogiri::XML(open(news_detail_url.call(news.external_id)).read)
         news.update_attributes(:published_at => DateTime.parse(news_detail.xpath('//pubDate').text))
-        print '.'
+        i += 1
+
+        puts "#{i}/#{news_count}"
       end
     end
 
