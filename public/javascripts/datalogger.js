bd0a9d5931bc099e9655ac5a5d6d080f6a40f867
Javier Arce <javierarce@gmail.com>
2012-02-09 13:12:23 -0500

    Adds event page for datalogger. Fixes problem with visited links on the homepage

--- public/javascripts/datalogger.js
+++ public/javascripts/datalogger.js
@@ -1,33 +1,5 @@
 $(function() {
 
-  
-	function getGeolocation(){
-	  alert('a');
-		if (google.loader.ClientLocation) {
-			userLatLng = new google.maps.LatLng(google.loader.ClientLocation.latitude, google.loader.ClientLocation.longitude);
-			showRoad(userLatLng,calculateNearOffice(userLatLng));
-	  } else {
-			bounds.extend(vizzuality_mad);
-			bounds.extend(vizzuality_ny);
-			map.fitBounds(bounds);
-			
-			vizz_marker = new google.maps.Marker({
-			    position: vizzuality_mad,
-					icon: vizz_icon,
-			    map: map
-			});
-			user_marker = new google.maps.Marker({
-			    position: vizzuality_ny,
-			    icon: vizz_icon,
-			    map: map
-			});
-			 
-			window.onresize = function(event) {
-			   map.setCenter(bounds.getCenter());
-			}      
-	  }
-	}
-
   $("a.reload").click(function(e){
     e.preventDefault();
     window.location.reload();
@@ -40,10 +12,11 @@ $(function() {
     }, 200);
 
   $("form.publish").bind('ajax:success', function() {
+    alert('a');
     window.location.reload();
   });
 
-  $("textarea").on("blur", function() {
+  $("textarea.toggle_header").on("blur", function() {
 
     $("header").show();
     $(".paper").css({top:headerHeight});
@@ -56,7 +29,7 @@ $(function() {
     }
   });
 
-  $("textarea").on("focus", function() {
+  $("textarea.toggle_header").on("focus", function() {
     $(".paper .placeholder").hide();
     $("header").hide();
     $(".paper").css({top:0});
